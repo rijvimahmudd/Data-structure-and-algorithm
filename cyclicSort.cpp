aaf3b15a9ea4017d8084ba47c9577b3b83feb8fd
rijvimahmudd <rijvimahmudd@gmail.com>
2023-09-19 15:06:11 +0600

    do cyclic sort

--- cyclicSort.cpp
+++ cyclicSort.cpp
@@ -7,16 +7,14 @@ void cyclicSort(vector<int>& nums, int& n){
     int i = 0;
     while (i < n) {
         int j = nums[i] - 1;
-
         if (nums[i] != nums[j] && nums[i] != i+1) swap(nums[i], nums[j]);
         else i++;
-
     }
 }
 
 int main(int argc, char const *argv[])
 {
-    vector<int> nums = {5,4,3,2,1};
+    vector<int> nums = {3,2,1,4,5};
     int n = nums.size();
     cyclicSort(nums, n);
     
